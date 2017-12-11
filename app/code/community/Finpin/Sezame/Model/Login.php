1552b91644d193bb9cd101ff35a7c6381816a0d3
Michael Bretterklieber <github@bretterklieber.com>
2017-12-11 16:48:13 +0100

    send fraud request only if sezame is enabled

--- app/code/community/Finpin/Sezame/Model/Login.php
+++ app/code/community/Finpin/Sezame/Model/Login.php
@@ -7,8 +7,20 @@ class Finpin_Sezame_Model_Login extends Mage_Core_Model_Observer
         Mage::getSingleton('core/session')->setSezameUsed(false);
     }
 
+    /**
+     * this method is invked after a login, send fraud alert if enabled and if sezame was not used
+     *
+     * @param $observer
+     */
     public function customerLogin($observer)
     {
+        /** @var Finpin_Sezame_Model_Auth $model */
+        $model = Mage::getModel('sezame/auth');
+
+        $sezameEnabled = $model->getConfigParam('settings/enabled');
+        if (!$sezameEnabled) {
+            return;
+        }
 
         if(Mage::getSingleton('customer/session')->isLoggedIn())
         {
@@ -20,8 +32,7 @@ class Finpin_Sezame_Model_Login extends Mage_Core_Model_Observer
 
             $customerData = Mage::getModel('customer/customer')->load($customer->getId());
 
-            /** @var Finpin_Sezame_Model_Auth $model */
-            $model = Mage::getModel('sezame/auth');
+
             if ($model->getConfigParam('settings/fraud'))
             {
                 /** @var Finpin_Sezame_Model_Link $linkModel */
